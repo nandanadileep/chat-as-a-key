44cff3b7d9f15732b5c134bc91f4402a977a0836
Nandana Dileep <nandanadileep29@gmail.com>
2026-05-02 16:55:28 +0530

    Re-export generic network collector and Playwright launch helpers from browser package

--- browser/__init__.py
+++ browser/__init__.py
@@ -1,5 +1,10 @@
 from .session_manager import BrowserSessionManager, SessionMode
 from .network_interceptor import ClaudeNetworkCollector
+from .generic_network_collector import GenericProviderNetworkCollector
+from . import headless_chrome
+from . import playwright_cleanup
+from .headless_chrome import launch_provider_browser, new_stealth_context
+from .playwright_cleanup import dispose_playwright_stack
 from .tracer import ObservabilityTracer, tracing_enabled
 from .cdp_observer import CdpReadOnlyObserver
 
@@ -9,6 +14,12 @@ __all__ = [
     "BrowserSessionManager",
     "SessionMode",
     "ClaudeNetworkCollector",
+    "GenericProviderNetworkCollector",
+    "headless_chrome",
+    "playwright_cleanup",
+    "launch_provider_browser",
+    "new_stealth_context",
+    "dispose_playwright_stack",
     "ObservabilityTracer",
     "tracing_enabled",
     "CdpReadOnlyObserver",
